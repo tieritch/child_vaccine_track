c57c12073cad4fae60a1ae8c574eec48779641d7
tieritch <tieritch@gmail.com>
2025-08-25 10:06:11 +0200

    correct a bug in knexfile

--- db/knexfile.js
+++ db/knexfile.js
@@ -3,6 +3,7 @@
  */
 const dotenv = require("dotenv");
 const path = require("path");
+dotenv.config({path:path.resolve(__dirname, "../.env")}); // to allow run migrations
 const baseConfig={
   client: 'pg',
   connection: {
