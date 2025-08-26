426dd7d8c00678171fde85ff1a4c92284b6223de
tieritch <tieritch@gmail.com>
2025-08-26 09:16:11 +0200

    feat(models/role.js): add Role model

---
+++ models/role.js
@@ -0,0 +1,57 @@
+const {Model}=require('objection');
+const path=require('path');
+module.exports=class Role extends Model{
+  static get tableName(){
+    return 'roles';
+  }
+  static get relationMappings(){
+    return {
+      users:{
+        relation:Model.ManyToManyRelation,
+        modelClass: require('./user'),
+        join:{
+          from:'roles.id',
+          through:{
+            from:'users_roles.role_id',
+            to:'users_roles.user_id',
+          },
+          to:'users.id',
+        },
+      },
+      permissions:{
+        relation:Model.ManyToManyRelation,
+        modelClass:require('./permission'),
+        join:{
+          from:'roles.id',
+          through:{
+            from:'roles_permissions_resources.role_id',
+            to:'roles_permissions_resources.permission_id',
+          },
+          to:'permissions.id',
+        },
+      },
+      resources:{
+        relation:Model.ManyToManyRelation,
+        modelClass: require('./resource'),
+        join:{
+          from:'roles.id',
+          through:{
+            from:'roles_permissions_resources.role_id',
+            to:'roles_permissions_resources.resource_id',
+          },
+          to:'resources.id',
+        },
+      },
+    };
+  }
+  $beforeInsert(){
+    if(this.name){
+      this.name=this.name.trim().toLowerCase();
+    }
+  }
+  $beforeUpdate(){
+    if(this.name){
+      this.name=this.name.trim().toLowerCase();
+    }
+  }
+};
