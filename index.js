d32a5d092aef05f15caf065936227c0ef2c5bd27
tieritch <tieritch@gmail.com>
2025-08-29 10:16:26 +0200

    add customFormatError key to graphqlHTTP handler

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express=require('express');
 const dotenv=require('dotenv');
 const cors=require('cors');
 const cookieParser=require('cookie-parser');
+const {graphqlHTTP}=require('express-graphql');
+const schema = require('./schema');
+const {rootResolver}=require('./resolvers');
 const app=express();
 app.use(express.json());
 app.use(cookieParser());
@@ -12,6 +15,37 @@ app.use(cors({
 dotenv.config();
 const knexInstance=require('./db/connection');
 const port=process.env.PORT||8000;
+app.use(express.json());
+app.use('/graphql', //graphqlValidator,
+    graphqlHTTP({
+      schema,
+      graphiql: true,
+      customFormatErrorFn: (err) => {
+      // Ici tu peux logger, envoyer à Sentry, etc.
+      console.error("GraphQL Error:", err.message);
+      return {
+        message: err.message,
+        code: err.originalError?.code || 500,
+      };
+  },
+}));
+/*app.use((err, req, res, next) => {
+
+  console.error(err.message);
+  const status = err.status || 500;
+
+  // If it's a validation error
+  if (err.details) {
+    return res.status(status).json({
+      error: 'Validation failed',
+      details: err.details,
+    });
+  }
+  // Otherwise, a generic server error
+  res.status(status).json({
+    error: err.message || 'Internal Server Error',
+  });
+});*/
 app.listen(port,()=>{
     console.log(`Node server listening on the port ${port}`)
 })
