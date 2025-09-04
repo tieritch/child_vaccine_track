913db8db6b7efde7c01c5a46df4a831e7ae2cfff
tieritch <tieritch@gmail.com>
2025-09-04 10:41:15 +0200

    add login mutaion

--- resolvers/user-resolver.js
+++ resolvers/user-resolver.js
@@ -1,8 +1,12 @@
+const bcrypt=require('bcrypt');
+const jwt=require('jsonwebtoken');
 const User = require('../models/user');
-const {formatJoiError}=require('../helpers');
+const {formatJoiError,isAuthenticated}=require('../helpers');
+const client=require('../redis-client');
 const {createUserSchema,
     updateUserSchema,
     deleteUserSchema,}=require('../validate');
+const { exist } = require('joi');
 const userResolver = {
   
    Query:{ 
@@ -14,8 +18,39 @@ const userResolver = {
         },
     },
     Mutation:{
+
+        login:async(_,input,context)=>{
+            const res=context.res;
+            const user=await User.query().findOne({username:input.username});
+            const accessToken = jwt.sign(user.toJSON(),process.env.ACCESS_TOKEN_SECRET,{expiresIn:'3600s'});
+            const refreshToken = jwt.sign(user.toJSON(),process.env.REFRESH_TOKEN_SECRET,{expiresIn:'1d'});
+ 
+            const existingToken=await client.get(`refresh_token:${user.id}`);
+            if(existingToken){
+                await client.del(`refresh_token:${user.id}`)
+            }
+            const hashedToken=await bcrypt.hash(refreshToken,10);
+            await client.set(`refresh_token:${user.id}`, hashedToken, { EX: 7 * 24 * 60 * 60 });
+            
+            res
+            .cookie("accessToken", accessToken, {
+                httpOnly: true,
+                maxAge: 60 * 60 * 1000,
+                secure: false,
+                sameSite: "Lax",
+            })
+            .cookie("refreshToken", refreshToken, {
+                httpOnly: true,
+                maxAge: 7 * 24 * 60 * 60 * 1000,
+                secure: false,
+                sameSite: "Lax",
+            });
+            return { accessToken, refreshToken, message: "Successful connexion!!" };
+        },
         
-        createUser: async (_,{input}) => {
+        createUser: async (_,{input},context) => {
+        
+            isAuthenticated(context);           
             try {
                 await createUserSchema.validateAsync(input, { abortEarly: false });
             } 
@@ -23,12 +58,13 @@ const userResolver = {
                 throw formatJoiError(err);
               }
             try{
+                console.log(input.operationName)
                 return await User.query().insert({
                 firstname:input.firstname,
                 lastname: input.lastname,
                 email:input.email,
                 username:input.username,
-                password:input.password
+                password:await bcrypt.hash(input.password,10)
             }).returning('*');
            
             } catch(err){
@@ -36,8 +72,8 @@ const userResolver = {
             }
         },
         
-        updateUser: async(_,{input})=>{
-            
+        updateUser: async(_,{input},context)=>{
+            isAuthenticated(context);
             try{
                 await updateUserSchema.validateAsync(input,{abortEarly:false});
             }
@@ -72,7 +108,7 @@ const userResolver = {
             catch(err){
                 throw new Error("Error while deleting the user: " + err.message);
             }
-        }
+        },
 
     }
 };
