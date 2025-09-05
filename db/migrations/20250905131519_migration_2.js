03fa487a2037f2e506feb56a81b07aaa8296ea1b
tieritch <tieritch@gmail.com>
2025-09-05 15:37:07 +0200

    add migration_2.js to migrations

---
+++ db/migrations/20250905131519_migration_2.js
@@ -0,0 +1,34 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up =  async function(knex) {
+   
+    await knex.schema
+        
+        .alterTable('children',(table)=>{
+            table.dropColumn('zone_id')
+        })
+
+        .createTable('zone_child_enrollments',(table)=>{
+            table.increments();
+            table.integer('children_id').references('id').inTable('children');
+            table.integer('zone_id').references('id').inTable('zones');
+            table.timestamps(true,true);
+        })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = async function(knex) {
+    
+    await knex.schema
+        
+        .alterTable('children',(table)=>{
+            table.addColumn('zone_id')
+        })
+        
+        .dropTableIfExists('zone_child_enrollments')
+};
