4ec66d0e4fb6dbefba6f9c052d4f28ab8a05efe7
tieritch <tieritch@gmail.com>
2025-09-10 15:50:33 +0200

    add zoneResolver

---
+++ resolvers/zone-resolver.js
@@ -0,0 +1,93 @@
+
+const Zone = require("../models/zone");
+const {formatJoiError,isAuthenticated}=require('../helpers');
+const {createZoneSchema,
+    updateZoneSchema,
+    deleteZoneSchema,}=require('../joi-schema');
+const { formatError } = require("graphql");
+
+const zoneResolver={
+
+    Query:{
+        getZone: async(_,{id})=>{
+            return await Zone.query().findById(id)
+        },
+        
+        zones: async()=>{
+            return await Zone.query();
+        }
+    },
+
+    Mutation:{
+
+        createZone: async(_,{input}, context)=>{
+
+            isAuthenticated(context);
+            try{
+                await createZoneSchema.validateAsync(input, {abortEarly: false});
+            }
+            catch(err){                
+                throw formatJoiError(err)
+            }
+
+            try{
+                const zone=await Zone.query().insert(input).returning('*');
+                return zone;
+            }
+            catch(err){
+                throw new Error(` Error while creating the zone:${err.message} ${err.message}`)
+            }
+        },
+
+        deleteZone: async(_, {id},context)=>{
+            
+            isAuthenticated(context);
+            try{
+                await deleteZoneSchema.validateAsync({id}, {abortEarly:false});
+            }
+            catch(err){
+                throw formatJoiError(err);
+            }
+            try{
+
+                let zone=await Zone.relatedQuery('agents').for(id).first();
+               // console.log(zone);
+                if(zone){
+                    throw new Error('can\'t delete the zone because agents are enrolled for this zone')
+                }
+                zone=await Zone.relatedQuery('enrollments').for(id).first();
+                if(zone){
+                    throw new Error('can\'t delete the zone because children are enrolled for this zone')
+                }
+                return await Zone.query().deleteById(id).returning('*');
+                
+            }
+            catch(err){
+                throw new Error(` Error while deleting the zone: ${err.message}`)
+            }
+        },
+
+        updateZone: async(_,{input})=>{
+            
+            isAuthenticated(context);
+            try{
+                await updateZoneSchema.validateAsync(input, {abortEarly:false})
+            }
+            catch(err){
+                formatError(err);
+            }
+            try{
+                const zoneNoId={...input};
+                delete zoneNoId.id;
+                const zone=await Zone.query().patch(zoneNoId).where({id: input.id}).first();
+                return zone;
+            }
+            catch(err){
+                throw new Error(`Error while updating the zone: ${err.message} `)
+            }
+        }
+    }
+
+}
+
+module.exports=zoneResolver;
