fb253e6ac57464773a992885eca29abf4f44fe95
tieritch <tieritch@gmail.com>
2025-09-10 08:49:39 +0200

    add zone-resolver module

--- graphql-schema/index.js
+++ graphql-schema/index.js
@@ -2,11 +2,12 @@ const {makeExecutableSchema}=require('@graphql-tools/schema');
 const {mergeTypeDefs,mergeResolvers}=require('@graphql-tools/merge');
 const userSDL=require('./SDL/user');
 const roleSDL=require('./SDL/role');
+const zoneSDL=require('./SDL/zone');
 const baseTypeDefs=require('./SDL/base')
 
-const typeDefs=mergeTypeDefs([baseTypeDefs,userSDL,roleSDL]);
-const {userResolver,roleResolver}=require('../resolvers')
-const resolvers = mergeResolvers([userResolver,roleResolver]);
+const typeDefs=mergeTypeDefs([baseTypeDefs,userSDL,roleSDL,zoneSDL]);
+const {userResolver,roleResolver, zoneResolver}=require('../resolvers')
+const resolvers = mergeResolvers([userResolver,roleResolver,zoneResolver]);
 
 const schema = makeExecutableSchema({
     typeDefs,
