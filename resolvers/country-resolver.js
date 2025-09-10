51a21c4100f08792e372343b09ac833206570fcc
tieritch <tieritch@gmail.com>
2025-09-10 15:52:25 +0200

    add countryResolver

---
+++ resolvers/country-resolver.js
@@ -0,0 +1,78 @@
+const Country=require('../models/country');
+const {formatJoiError,isAuthenticated}=require('../helpers');
+const {createCountrySchema,
+    updateCountrySchema,
+    deleteCountrySchema,}=require('../joi-schema');
+
+const countryResolver={
+
+    Query:{
+        
+        getCountry: async(_, {id} )=>{
+            return await Country.query().findById(id);
+        },
+
+        countries: async()=>{
+            return await Country.query();
+        }
+    },
+
+    Mutation:{
+
+        createCountry: async(_,{input},context)=>{
+            
+            try{
+                await createCountrySchema.validateAsync(input,input, { abortEarly: false });
+            }
+            catch(err){
+                throw formatJoiError(err);
+            }
+
+            try{
+                isAuthenticated(context);
+                return await Country.query().insert(input).returning('*');
+            }
+            catch(err){
+                throw new Error(` Error while creating the coutry:${err.message}`)
+            }
+        },
+
+        deleteCountry: async(_,{id},context)=>{
+            
+            try{
+                await deleteCountrySchema.validateAsync({id},{abortEarly: false});
+            }
+            catch(err){
+                throw formatJoiError(err)
+            }
+
+            try{
+                isAuthenticated(context);
+                return await Country.query().deleteById(id).returning('*');
+            }
+            catch(err){
+                throw new Error(` Error while deleting the country: ${err.message}`)
+            }
+        },
+
+         updateCountry: async(_,{input},context)=>{
+        
+            try{
+                await updateCountrySchema.validateAsync(input, {abortEarly: false})
+            }
+            catch(err){
+                throw formatJoiError(err);
+            }
+
+            try{
+                let countryNoId={...input};
+                delete countryNoId.id;    
+                return await Country.query().patch(countryNoId).where({id:input.id}).returning('*').first();
+            }
+            catch(err){
+                throw new Error(` Error while updating the country: ${err.message}`);
+            }
+    }}
+}
+
+module.exports=countryResolver;
