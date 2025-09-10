8d5c155e089e5801bb04229241abe3c2dc4f24a2
tieritch <tieritch@gmail.com>
2025-09-10 10:43:14 +0200

    correct a bug

--- models/health-agent.js
+++ models/health-agent.js
@@ -1,5 +1,8 @@
 const {Model}=require('objection');
 
-module.exports=class HelathAgent extends Model{
-    
+module.exports=class HealthAgent extends Model{
+     
+    static get tableName(){
+        return "health_agents"
+    }
 }
