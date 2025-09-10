9515510cd7465cdad6648d8a6529ffea29c72af0
tieritch <tieritch@gmail.com>
2025-09-10 08:57:31 +0200

    add Zone model

---
+++ models/zone.js
@@ -0,0 +1,48 @@
+const {Model}=require('objection');
+
+module.exports=class Zone extends Model{
+
+    static get tableName(){
+        return "zones";
+    } 
+
+    static get relationMappings(){
+       
+        return {
+            
+            enrollments:{
+                relation:  Model.ManyToManyRelation,
+                modelClass: require('./enrollment'),
+                from:"zones.id",
+                through:{
+                    from: "zone_child_enrollments.zone_id",
+                    to:"zone_child_enrollments.children_id"
+                },
+                to:'children.id'
+            },
+            
+            agents:{
+                relation: Model.ManyToManyRelation,
+                modelClass:require('./health-agent'),
+                from:'zones.id',
+                    through:{
+                        from:'zones_agents.zone_id',
+                        to:'zones_agents.agent_id'
+                    },
+                to:'health_agents.id',
+            }
+        }
+    }
+
+    $beforeInsert(){
+        if(this.name){
+          this.name=this.name.trim().toLowerCase();
+        }
+      }
+    
+    $beforeUpdate(){
+        if(this.name){
+          this.name=this.name.trim().toLowerCase();
+        }
+      }
+}
