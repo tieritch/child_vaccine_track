b5949efe0fd09365842527b9360f34ebffd40494
tieritch <tieritch@gmail.com>
2025-09-12 18:24:42 +0200

    implement auth logic on schema

--- graphql-schema/index.js
+++ graphql-schema/index.js
@@ -2,6 +2,7 @@ const {makeExecutableSchema}=require('@graphql-tools/schema');
 const {mergeTypeDefs,mergeResolvers}=require('@graphql-tools/merge');
 const { mapSchema, getDirective, MapperKind } = require('@graphql-tools/utils');
 const { defaultFieldResolver } = require('graphql');
+const {authenticate}=require('../helpers');
 
 const baseTypeDefs=require('./SDL/base');
 const directiveSDL=require('./SDL/directive');
@@ -14,8 +15,22 @@ const typeDefs=mergeTypeDefs([baseTypeDefs,directiveSDL,userSDL,roleSDL,zoneSDL,
 const {userResolver,roleResolver, zoneResolver,countryResolver}=require('../resolvers')
 const resolvers = mergeResolvers([userResolver,roleResolver,zoneResolver,countryResolver]);
 
-const schema = makeExecutableSchema({
+let schema = makeExecutableSchema({
     typeDefs,
     resolvers,
+});
+
+schema = mapSchema(schema, {
+  [MapperKind.OBJECT_FIELD]: (fieldConfig) => {
+    const directive = getDirective(schema, fieldConfig, 'auth')?.[0];
+    if (directive) {
+      const { resolve = defaultFieldResolver } = fieldConfig;
+      fieldConfig.resolve = async (source, args, context, info) => {
+        authenticate(context);
+        return resolve(source, args, context, info);
+      };
+    }
+    return fieldConfig;
+  }
 });
 module.exports=schema;
