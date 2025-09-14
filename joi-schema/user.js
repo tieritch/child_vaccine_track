634fdc541caad8d64e5e10f6b673264b2d497c0a
tieritch <tieritch@gmail.com>
2025-09-14 08:56:46 +0200

    refactor createUserSchema

--- joi-schema/user.js
+++ joi-schema/user.js
@@ -1,5 +1,6 @@
 const Joi=require('joi');
 const User=require('../models/user');
+const Role=require('../models/role')
 const bcrypt=require('bcrypt');
 
 const loginSchema=Joi.object({
@@ -28,13 +29,19 @@ const createUserSchema = Joi.object({
       .messages({
         "string.pattern.base": "Password must contain at least one uppercase letter, one lowercase letter, and one number."
       }),
-    username: Joi.string().min(6).required()
+    username: Joi.string().min(6).required(),
+    role_ids:Joi.array()
 })
 .external( async(input)=>{
     let exists=await User.query().findOne({email:input.email});
     if (exists) throw new Error("Email in use");
     exists=await User.query().findOne({username:input.username});
     if (exists) throw new Error("Username in use");
+    const roles=await Role.query();
+    if(input.role_ids && input.role_ids.length>0){
+    const validRoleIds=input.role_ids.filter(roleId=>roles.map(role=>role.id).includes(parseInt(roleId)));
+    input.role_ids=validRoleIds
+    }
 })
 
 const updateUserSchema=Joi.object({
