3e10dbabc72eb2f676b5be8412b4f36ad405a963
tieritch <tieritch@gmail.com>
2025-09-15 12:37:27 +0200

    refactor updateSchemaRole and createSchemaRole

--- joi-schema/role.js
+++ joi-schema/role.js
@@ -8,6 +8,11 @@ createRoleSchema=Joi.object({
     permission_ids: Joi.array(),
     resource_ids: Joi.array()
 }).external( async(input)=>{
+    if(input.permission_ids && input.permission_ids.length>0){
+        if(!input.resource_ids || input.resource_ids.length==0){
+            throw new Error('resource IDs required');
+        }
+    }
     const role=await Role.query().findOne({name:input.name.trim()});
     if(role){
         throw new Error("The role already exists");
@@ -34,8 +39,15 @@ updateRoleSchema=Joi.object({
     permission_ids:Joi.array(),
     resource_ids: Joi.array()
 }).external( async(input)=>{
-    const role=await Role.query().findOne({name:input.name.trim().toLowerCase()});
-    
+    if(input.permission_ids && input.permission_ids.length>0){
+        if(!input.resource_ids || input.resource_ids.length==0){
+            throw new Error('resource IDs required');
+        }
+    }
+    let role=null;
+    if(input.name){
+        role=await Role.query().findOne({name:input.name.trim().toLowerCase()});
+    }
     if(role){
         if(role.name=="admin"){
             throw new Error("Can't modify the super admin role ");
@@ -43,15 +55,17 @@ updateRoleSchema=Joi.object({
         throw new Error("The role already exists");
     }
     const existingPerms=await Permission.query();
-    const validPermIds=input.permisson_ids?.filter(id=> existingPerms.map(perm=>perm.id).includes(parceInt(id)));
+    const validPermIds=input.permission_ids?.filter(id=> existingPerms.map(perm=>perm.id).includes(parseInt(id)));
     if(validPermIds?.length==0){
         throw new Error("No valid permission id provided");
     }
+    input.permission_ids=validPermIds;// replace all ids with only valid  ids
     const existingResources=await Resource.query();
     const validResourceIds=input.resource_ids?.filter( id=> existingResources.map(res=>res.id).includes(parseInt(id)));
     if(validResourceIds?.length==0){
         throw new Error("No valid resource id provided");
     }
+    input.resource_ids=validResourceIds; // replace all ids with only valid  ids
 });
 
 deleteRoleSchema=Joi.object({
