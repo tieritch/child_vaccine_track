58f470dccfc74e4bfa0dc04e0568938215ff8b61
tieritch <tieritch@gmail.com>
2025-09-15 12:38:49 +0200

    correct a bug in Permission model

--- models/permission.js
+++ models/permission.js
@@ -12,12 +12,24 @@ module.exports=class Permission extends Model{
         join:{
           from:'permissions.id',
           through:{
-            from:'roles_permissions.permission_id',
-            to:'roles_permissions.role_id',
+            from:'roles_permissions_resources.permission_id',
+            to:'roles_permissions_recources.role_id',
           },
           to:'roles.id',
         },
       },
+      resources:{
+        relation: Model.ManyToManyRelation,
+        modelClass: require('./resource'),
+        join:{
+          from:"permissions.id",
+          through:{
+            from:"roles_permissions_resources.permission_id",
+            to:"roles_permissions_resources.resource_id"
+          },
+          to:"resources.id"
+        }
+      }
     };
   }
 };
