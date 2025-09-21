08aa107acc6c2dff7cfe113c90c0718f2a883515
tieritch <tieritch@gmail.com>
2025-09-21 16:28:49 +0200

    import parent module

--- joi-schema/index.js
+++ joi-schema/index.js
@@ -6,9 +6,12 @@ const {createZoneSchema,updateZoneSchema,deleteZoneSchema}=require('./zone');
 
 const {createCountrySchema,updateCountrySchema,deleteCountrySchema}=require('./country');
 
+const { createParentSchema,updateParentSchema,deleteParentSchema}=require('./parent');
+
 module.exports={
     createUserSchema,    updateUserSchema,    deleteUserSchema, loginSchema,
     createRoleSchema,    updateRoleSchema,    deleteRoleSchema,
     createZoneSchema,    updateZoneSchema,    deleteZoneSchema,
-    createCountrySchema, updateCountrySchema, deleteCountrySchema
+    createCountrySchema, updateCountrySchema, deleteCountrySchema,
+    createParentSchema,  updateParentSchema,  deleteParentSchema  
 }
