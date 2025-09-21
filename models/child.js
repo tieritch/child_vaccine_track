f5b0768b0d1329b2570589a2fd800f867ac0021b
tieritch <tieritch@gmail.com>
2025-09-21 12:26:20 +0200

    add Child model:

--- models/child.js
+++ models/child.js
@@ -5,4 +5,52 @@ module.exports=class Child extends Model{
      static get tableName(){
         return "children"
      }
+
+     static get relationMappings(){
+       
+         return {
+            
+            zones:{
+               relation: Model.ManyToManyRelation,
+               modelClass: require('./zone'),
+               join:{
+                  from:" children.id",
+                  through:{
+                     from:"zone_child_enrollments.children_id",
+                     to:"zone_child_enrollments.zone_id",
+                  },
+                  to:"zones.id"
+               }
+            },
+
+            parent:{
+               relation: Model.BelongsToOneRelation,
+               modelClass: require('./parent'),
+               join:{
+                  from: "children.parent_id",
+                  to:" parents.id"
+               }
+            }
+         } 
+     }
+
+     $beforeInsert(){
+      if(this.firstname){
+        this.firstname=this.firstname.trim().toLowerCase();
+      }
+      if(lastname){
+         this.lastname=this.lastname.trim().toLowerCase();
+      }
+      if(sex){
+         this.sex=this.sex.trim().toLowerCase();
+      }
+    }
+    $beforeUpdate(){
+      if(this.firstname){
+         this.firstname=this.firstname.trim().toLowerCase();
+       }
+       if(lastname){
+          this.lastname=this.lastname.trim().toLowerCase();
+       }
+    }
 }
