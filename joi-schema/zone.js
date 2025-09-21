3b6aadc18c05b0ed2beb3a121e7f2f14c8ffee2f
tieritch <tieritch@gmail.com>
2025-09-21 09:09:42 +0200

    define deleteZoneSchema validation

--- joi-schema/zone.js
+++ joi-schema/zone.js
@@ -36,7 +36,24 @@ const updateZoneSchema=Joi.object({
     }
 });
 
-const deleteZoneSchema=Joi.object();
+const deleteZoneSchema=Joi.object({
+   id:Joi.number().integer().required()
+}).external( async({id})=>{
+
+    let zone=await Zone.query().findById(id);
+    if(!zone){
+        throw new Error(' The zone ID does not exist')
+    }
+    
+    const childEnroll=await Zone.relatedQuery('childEnrolls').for(id).first();
+    if( childEnroll){
+        throw new Error('Cannot delete zone: children enrolled associated with this zone exist'); 
+    }
+    const agent = await Zone.relatedQuery('agents').for(id).first();
+    if(agent){
+        throw new Error('Cannot delete zone: Health agents associated with this zone exist'); 
+    }
+} )
 
 module.exports={
     createZoneSchema,
