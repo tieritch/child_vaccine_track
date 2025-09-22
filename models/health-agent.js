43f567f37793081779c07149ba02a5a39d4618d2
tieritch <tieritch@gmail.com>
2025-09-22 17:18:18 +0200

    add relationMapings

--- models/health-agent.js
+++ models/health-agent.js
@@ -5,4 +5,20 @@ module.exports=class HealthAgent extends Model{
     static get tableName(){
         return "health_agents"
     }
+
+    static get relationMappings(){
+
+        return{
+            zone:{
+                relation: Model.ManyToManyRelation,
+                modelClass: require('./health-agent'),
+                from:"health_agents.id",
+                through:{
+                    from:" zones_agents.agent_id",
+                    to:"zones_agents.zone_id"
+                },
+                to: "zones.id"
+            }
+        }
+    }
 }
