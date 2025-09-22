9c70b55426960e32d6c93023e7c3b9d88868b8be
tieritch <tieritch@gmail.com>
2025-09-22 16:56:36 +0200

    correct bugs

--- graphql-schema/SDL/child.js
+++ graphql-schema/SDL/child.js
@@ -5,16 +5,17 @@ const childSDL=`
         firstname: String!
         lastname: String
         address: String!
-        sex: String!
-        birth_year: String!
+        sex_id: ID!
+        birth_date: String!
+        parent_id: ID!
     }
     
     input ChildInput{
         firstname: String!
         lastname: String
         address: String!
-        sex: String!
-        birth_year: String! 
+        sex_id: ID!
+        birth_date: String! 
         parent_id: ID!
 
     }
@@ -24,19 +25,19 @@ const childSDL=`
         firstname: String
         lastname: String
         address: String
-        sex: String
-        birth_year: String      
+        sex_id: ID
+        birth_date: String      
         parent_id: ID
 
     }
 
     extend type Query{
-        getChild(id:ID!): Child!
-        children: [Child!]!
+        getChild(id:ID!): Child
+        children: [Child!]
     }
     extend type Mutation{
-        createChild( input: ChildInput!): Child! @auth @rbac( actions:["READ","CREATE"], resources:["children"])
-        updateChild( input: UpdChildInput!): Child! @auth @rbac( actions:["READ","UPDATE"], resources:["children"])
+        createChild( input: ChildInput!): Child @auth @rbac( actions:["READ","CREATE"], resources:["children"])
+        updateChild( input: UpdChildInput!): Child @auth @rbac( actions:["READ","UPDATE"], resources:["children"])
         deleteChild(id:ID!): Child @auth @rbac( actions:["READ","DELETE"], resources:["children"])
     }
 `
