0cf65d641484f19094f5b08a08df20c081e0bc06
tieritch <tieritch@gmail.com>
2025-09-22 17:01:34 +0200

    add childResolver

---
+++ resolvers/child-resolver.js
@@ -0,0 +1,87 @@
+
+const Child=require('../models/child');
+const Parent=require('../models/parent');
+const {formatJoiError}=require('../helpers');
+const {
+    createChildSchema,
+    updateChildSchema,
+    deleteChildSchema
+}
+=require('../joi-schema');
+const { ErrorReply } = require('redis');
+
+const childResolver={
+
+    Query:{
+
+        getChild: async(_, {id})=>{
+            return await Child.query().findById(id);
+        },
+
+        children: async()=>{
+            return await Child.query();
+        }
+
+    },
+
+    Mutation:{
+
+        createChild: async(_, {input})=>{
+            try{
+                await createChildSchema.validateAsync(input, { abortEarly: false });
+            }
+            catch(err){
+                return formatJoiError(err);
+            }
+
+            try{
+                 return await Child.query().insert(input).returning('*');
+            }
+            catch(err){
+                throw new Error(` Error while creating the child: ${err.message}`)
+            }
+        },
+
+        updateChild: async(_, {input})=>{
+            
+            try{
+                await updateChildSchema.validateAsync(input, {abortEarly: false});
+            }
+            catch(err){
+                return formatJoiError(err)
+            }
+
+            try{
+                const childNoId={...input};
+                delete childNoId.id
+               return await Child.query().patch(childNoId).where({id:input.id}).returning('*').first()
+            }
+            catch(err){
+                throw new Error(` Error while updating the child: ${err.message}`)
+            }
+        },
+
+        deleteChild: async(_,{id})=>{
+        
+            try{
+                await deleteChildSchema.validateAsync({id}, {abortEarly:false});
+            }
+            catch(err){
+                return formatJoiError(err);
+            }
+    
+            try{
+                return await Child.query().deleteById(id).returning('*');
+            }
+            catch(err){
+                throw new Error(`Error while deleting the child: ${err.message} `)
+            }
+    
+        }
+
+    }
+
+    
+}
+
+module.exports=childResolver;
