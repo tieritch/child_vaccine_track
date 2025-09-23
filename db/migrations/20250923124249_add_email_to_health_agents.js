f5806c6063832f8ef1cc9a917ab6b7935de0d3c1
tieritch <tieritch@gmail.com>
2025-09-23 14:46:25 +0200

    alter health_agents table by adding email column

---
+++ db/migrations/20250923124249_add_email_to_health_agents.js
@@ -0,0 +1,21 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  
+    return knex.schema.alterTable('health_agents',(table)=>{
+        table.string('email').notNullable().unique();
+    })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  
+    return knex.schema.alterTable('health_agents', (table)=>{
+        table.dropColumn('email');
+    })
+};
