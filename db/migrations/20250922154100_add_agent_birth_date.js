6845ed4126195f251654b9edd0b87d3c06064636
tieritch <tieritch@gmail.com>
2025-09-23 09:28:42 +0200

    alter health_agents table by adding birth_date column

---
+++ db/migrations/20250922154100_add_agent_birth_date.js
@@ -0,0 +1,21 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+  
+    return knex.schema.alterTable('health_agents',(table)=>{
+        table.date('birth_date').notNullable();
+    })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  
+    return knex.schema.alterTable('health_agents', (table)=>{
+        table.dropColumn('birth_date');
+    })
+};
