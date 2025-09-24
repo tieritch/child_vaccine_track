6464fa35d144f0e68e83b780cc1fa7787fddd978
tieritch <tieritch@gmail.com>
2025-09-24 17:01:39 +0200

    remove comments

--- resolvers/date-resolver.js
+++ resolvers/date-resolver.js
@@ -4,19 +4,16 @@ const { GraphQLScalarType, Kind } = require('graphql');
 const DateScalar = new GraphQLScalarType({
   name: 'Date',
   description: 'Custom scalar type for Dates',
+  
   serialize(value) {
-    // sortie -> client
-    console.log(' EN DATE SCALAR SERIALIZE_____________')
     return value instanceof Date ? value.toISOString() : null;
   },
+
   parseValue(value) {
-    // entrée -> mutation variables
-    console.log(' EN DATE SCALAR PARSEVALUE____________')
     return new Date(value);
   },
+  
   parseLiteral(ast) {
-    // entrée -> inline string
-    console.log(' EN DATE SCALAR LITERAL____________')
     if (ast.kind === Kind.STRING) {
       return new Date(ast.value);
     }
