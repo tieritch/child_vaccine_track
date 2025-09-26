8aff750d4a5f01589e190540c663ab11338cda7b
tieritch <tieritch@gmail.com>
2025-09-26 09:43:53 +0200

    remove models/zone-child-enrollment.js

--- models/zone-child-enrollment.js
+++
@@ -1,7 +0,0 @@
- const {Model}=require('objection');
- module.exports=class ZoneChildEnrollment extends Model{
-
-        static get  tableName(){
-            return "zone_child_enrollments"
-        }
- }
