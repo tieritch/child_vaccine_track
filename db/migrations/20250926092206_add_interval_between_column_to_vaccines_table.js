a5a4edd941aab514e41ac32118eac2f0594bf5e5
tieritch <tieritch@gmail.com>
2025-09-26 11:32:58 +0200

    add interval_between column to vaccines table

---
+++ db/migrations/20250926092206_add_interval_between_column_to_vaccines_table.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+    return knex.schema.alterTable('vaccines', (table)=>{
+        table.integer('interval_between');
+    })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+    return knex.schema.alterTable('vaccines', (table)=>{
+        table.dropColumn('interval_between');
+    })
+};
